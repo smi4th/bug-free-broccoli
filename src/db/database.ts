e43c1793a3169c73c390befb36dcf73f9d154212
smiath <smiath.yt@gmail.com>
2025-03-20 16:16:13 +0100

    feat: add init db with typeorm

---
+++ src/db/database.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "admin",
+  password: "admin",
+  database: "mydatabase",
+  logging: true,
+  synchronize: true
+})
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { initHandlers } from "./handlers/handler";
+import { AppDataSource } from "./db/database";
 
-const app = () => {
+const app = async () => {
   const app = express();
   const port = 3000;
   app.use(express.json());
   initHandlers(app);
+
+  try {
+    await AppDataSource.initialize();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
 
   app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
